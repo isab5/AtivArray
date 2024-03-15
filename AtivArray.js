e552980f27f8f2f6901bc1a088cf9f53aa95c5e4
Isabella <isabellaborin.tds1.senai@gmail.com>
2024-03-15 19:33:23 -0300

    Adicionei o método .concat()

--- AtivArray.js
+++ AtivArray.js
@@ -1,16 +1,22 @@
 // primeiro eu criei uma variável
 let heroisMarvel = ['homem de ferro', 'hulk', 'feiticeira escarlate', 'capita marvel'];
 
-// primeiro método adicionado é o método .reverse() que inverte o array.
+// O primeiro método adicionado é o método .reverse() que inverte o array.
 heroisMarvel.reverse();
 
 //O segundo método é o .slice(), ele seleciona uma parte de um array e retorna um novo array.
 let limite = heroisMarvel.slice(2, 5);
 
 // O terceito método é o .join(), ele recebe um parâmetro que separar os elementos do array.
-let parametro = heroisMarvel.join(" - ")
+let parametro = heroisMarvel.join(" - ");
+
+// O quarto método é o .concat(), ele junta dois ou mais arrays e retorna um novo array.
+let viloesMarvel = ['thanos', 'loki', 'hela', 'ultron'];
+
+let universoMarvel = heroisMarvel.concat(viloesMarvel);
 
 //Espaço reservado para os consoles.
 console.log(heroisMarvel);
 console.log(limite);
 console.log(parametro);
+console.log(universoMarvel);
