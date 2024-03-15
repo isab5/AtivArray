a26b58a3b7b5c5afd86a221e1f486020fab223c3
Isabella <isabellaborin.tds1.senai@gmail.com>
2024-03-15 17:31:43 -0300

    Método .join() adicionado

--- AtivArray.js
+++ AtivArray.js
@@ -6,5 +6,11 @@ heroisMarvel.reverse();
 
 //O segundo método é o .slice(), ele seleciona uma parte de um array e retorna um novo array.
 let limite = heroisMarvel.slice(2, 5);
+
+// O terceito método é o .join(), ele recebe um parâmetro que separar os elementos do array.
+let parametro = heroisMarvel.join(" - ")
+
+//Espaço reservado para os consoles.
 console.log(heroisMarvel);
 console.log(limite);
+console.log(parametro);
